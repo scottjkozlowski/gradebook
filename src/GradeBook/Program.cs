3be41771b6b14f3e029fae6c70ed79a79320c934
Kozlowski, Scott <10034024@id.ohio.gov>
2020-02-20 15:24:39 -0500

    completed unit test addition

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -13,17 +13,11 @@ namespace GradeBook
             book.AddGrade(90.5);
             book.AddGrade(77.5);
 
-            var grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
-            grades.Add(56.1); 
+            var stats = book.GetStatistics();
+            Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The highest grade is {stats.High:N1}");
+            Console.WriteLine($"The average grade is {stats.Average:N1}");
 
-            var result = 0.0;
-            foreach(var number in grades)
-            {
-                
-                result += number;
-            } 
-            result /= grades.Count;
-            Console.WriteLine($"The average grade is {result:N1}");
-        }        
+        }
     }
 }
